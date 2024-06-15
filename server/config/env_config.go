6f8661184b5c8840ee06d9ed289a27a481893089
lesedipitsi123 <lesedipitsi@gmail.com>
2024-06-15 23:24:15 +0200

    feat: add env config

--- server/config/env_config.go
+++ server/config/env_config.go
@@ -1,1 +1,14 @@
 package config
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func SetupEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading enviornment variables")
+	}
+}
