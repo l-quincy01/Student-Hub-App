6dffd1a9a5c70f5fe904774faebbfdb6eef5d579
lesedipitsi123 <lesedipitsi@gmail.com>
2024-06-15 23:25:47 +0200

    feat: add main file for api

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"context"
+	"student-hub-app/config"
+)
+
+func init() {
+	config.SetupEnv()
+}
+
+func main() {
+	ctx := context.Background()
+
+	client := config.SetupMongoDb(ctx)
+	defer config.CloseMognoDb(ctx, client)
+}
